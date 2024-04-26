162ee2e5888bc89089acb921a59c8e53c16eb9e9
Abdul Kabeer <abdulkabeer372@gmail.com>
2024-04-26 10:37:45 +0500

    Personal Message code

---
+++ index.js
@@ -0,0 +1,5 @@
+"use strict";
+// Store a person’s name in a variable, and print a message to that person.
+//Your message should be simple, such as, “Hello Eric, would you like to learn some Python today?”
+let personName = "kumail";
+console.log(`Hello ${personName}, how are you doing today?`);
