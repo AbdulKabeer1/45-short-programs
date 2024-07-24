6aff463e0af7c582583c3094e555b5117f57d137
Abdul Kabeer <163725303+AbdulKabeer1@users.noreply.github.com>
2024-07-24 22:01:00 +0500

    Delete index.js

--- index.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-// Store a person’s name in a variable, and print a message to that person.
-//Your message should be simple, such as, “Hello Eric, would you like to learn some Python today?”
-let personName = "kumail";
-console.log(`Hello ${personName}, how are you doing today?`);
